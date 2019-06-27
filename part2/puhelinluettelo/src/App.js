f93505d1968e964bbb3ac73204438e9294c7278a
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-06-27 12:33:58 +0300

    Puhelinluettelo-exercise to use axios

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Personform from "./components/Personform";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterString, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/persons").then(response => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addName = event => {
     event.preventDefault();
@@ -43,7 +45,7 @@ const App = () => {
   };
 
   return (
-    <div class="App">
+    <div className="App">
       <h2>Phonebook</h2>
       <Filter onChange={handleFilterChange} filter={filterString} />
       <Personform
