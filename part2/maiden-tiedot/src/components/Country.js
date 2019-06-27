1f6bb168d83e141f3ae4e0c191238eacba9d60c0
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-06-27 17:00:19 +0300

    Working temperature

--- part2/maiden-tiedot/src/components/Country.js
+++ part2/maiden-tiedot/src/components/Country.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [currWeather, setWeather] = useState("");
+  const getWeather = location => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          location +
+          "&APPID=dea560842d3e852a682f3ea93448017a"
+      )
+      .then(response => {
+        setWeather((response.data.main.temp - 273.15).toFixed(1));
+      });
+  };
+
+  useEffect(() => {
+    getWeather(country.name);
+  }, []);
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -17,6 +35,8 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flag} alt="flag" className="flag" />
+      <h3>Weather in {country.capital}</h3>
+      <p>{currWeather} &deg;C</p>
     </div>
   );
 };
