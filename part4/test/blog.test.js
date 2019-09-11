5edad03f3e23fd49fffe94b3930d6c02c900dfc2
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-09-11 22:31:00 +0300

    Finished 4.7 and everything before it

--- part4/test/blog.test.js
+++ part4/test/blog.test.js
@@ -8,7 +8,7 @@ test('dummy returns one', () => {
 });
 
 describe('total likes', () => {
-  const listWithOneBlog = [
+  const listWithBlogs = [
     {
       _id: '5a422aa71b54a676234d17f8',
       title: 'Go To Statement Considered Harmful',
@@ -17,11 +17,151 @@ describe('total likes', () => {
         'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
       __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 13,
+      __v: 0
     }
   ];
 
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(5);
+  test('Total likes function to return a correct number', () => {
+    const result = listHelper.totalLikes(listWithBlogs);
+    expect(result).toBe(18);
+  });
+
+  test('Returns an empty array if no blogs are given', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual([]);
+  });
+});
+
+describe('favorite blog', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Some text here',
+      author: 'Edsger W. Wijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 13,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Other text here',
+      author: 'Edsger W. Zijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 200,
+      __v: 0
+    }
+  ];
+
+  test('FavoriteBlog function to return the author with the blog with most votes', () => {
+    const result = listHelper.favoriteBlog(listWithBlogs);
+    expect(result).toBe(listWithBlogs[2]);
+  });
+
+  test('Returns an empty array if no blogs are given', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual([]);
+  });
+});
+
+describe('most blogs', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Some text here',
+      author: 'Edsger W. Wijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 13,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Other text here',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 200,
+      __v: 0
+    }
+  ];
+
+  test('mostLikes function to return the author with the blog with most likes', () => {
+    const result = listHelper.mostBlogs(listWithBlogs);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 2 });
+  });
+
+  test('Returns an empty array if no blogs are given', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual([]);
+  });
+});
+
+describe('most likes', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Some text here',
+      author: 'Edsger W. Wijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 13,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Other text here',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 200,
+      __v: 0
+    }
+  ];
+
+  test('mostLikes function to return the author with the blog with most likes', () => {
+    const result = listHelper.mostLikes(listWithBlogs);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', likes: 205 });
+  });
+
+  test('Returns an empty array if no blogs are given', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual([]);
   });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,12 +1,46 @@
+const _ = require('lodash');
+
 const dummy = blogs => {
   return 1;
 };
 
 const totalLikes = blogs => {
+  if (blogs.length === 0) return [];
   return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+};
+
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return [];
+  return blogs.reduce((acc, curr) => (curr.likes > acc.likes ? curr : acc));
+};
+
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return [];
+  const grouped = _.countBy(blogs, a => a.author);
+  return Object.entries(grouped).reduce((acc, curr) => {
+    if (!acc || acc.blogs < curr[1]) {
+      return { author: curr[0], blogs: curr[1] };
+    } else {
+      return acc;
+    }
+  }, false);
+};
+
+const sumLikes = arr => {
+  return arr ? arr.reduce((acc, curr) => acc + curr.likes, 0) : 0;
+};
+
+const mostLikes = blogs => {
+  if (blogs.length == 0) return [];
+  return Object.entries(_.groupBy(blogs, a => a.author))
+    .map(i => ({ author: i[0], likes: sumLikes(i[1]) }))
+    .sort((a, b) => b.likes - a.likes)[0];
 };
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
