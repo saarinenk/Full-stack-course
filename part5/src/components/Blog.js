3ea3563df932d9dbf99b90c1ef0c0718bced71c1
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-12-26 22:11:47 +0200

    Expandable blogs (5.6)

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+import '../App.css';
+
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  console.log(blog);
+
+  const hide = { display: 'none' };
+
+  return (
+    <div className='blog'>
+      <div onClick={toggleOpen}>
+        <b>
+          {blog.title} – {blog.author}
+        </b>
+      </div>
+      <div style={open ? {} : hide}>
+        <a href={blog.url}>{blog.url}</a>
+        <div className='inline'>
+          <p>Likes: {blog.likes}</p>
+          <button>like</button>
+        </div>
+        <p>Added by: {blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
