7d7f9983df97d1363c454e2c20fbc55d41d7c7d9
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-12-29 18:15:22 +0200

    Also anecdote listing to its own component (6.8)

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
-import { voteAnecdote } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = props => {
-  const anecdotes = props.store.getState();
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote => (
-          <div key={anecdote.id + anecdote.votes}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button
-                onClick={() => props.store.dispatch(voteAnecdote(anecdote.id))}
-              >
-                vote
-              </button>
-            </div>
-          </div>
-        ))}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   );
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = props => {
+  const anecdotes = props.store.getState();
+
+  return (
+    <div>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote => (
+          <div key={anecdote.id + anecdote.votes}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button
+                onClick={() => props.store.dispatch(voteAnecdote(anecdote.id))}
+              >
+                vote
+              </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
