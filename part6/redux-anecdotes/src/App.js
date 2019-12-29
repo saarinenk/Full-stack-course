79dd5990346c7c7c8feb9a76aaaa6243cf8736fb
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-12-29 17:42:56 +0200

    Added a possibility to vote anecdotes (6.3)

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import { voteAnecdote } from './reducers/anecdoteReducer';
 
-const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+const App = props => {
+  const anecdotes = props.store.getState();
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button
+              onClick={() => props.store.dispatch(voteAnecdote(anecdote.id))}
+            >
+              vote
+            </button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
       <form>
-        <div><input /></div>
+        <div>
+          <input />
+        </div>
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,25 +5,40 @@ const anecdotesAtStart = [
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
+const asObject = anecdote => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0
-  }
-}
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id;
+      const anecdoteToVote = state.find(anecdote => anecdote.id === id);
+      const votedA = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      };
+      return state.map(anecdote => (anecdote.id === id ? votedA : anecdote));
+    default:
+      return state;
+  }
+};
 
-  return state
-}
+export const voteAnecdote = id => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  };
+};
 
-export default reducer
+export default reducer;
